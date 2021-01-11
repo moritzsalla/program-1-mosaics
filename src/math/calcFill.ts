13354768ffd8ceebf813037e9de3fb68a19d1f4e
moritzsalla <moritz.salla@hotmail.de>
2021-01-11 12:12:39 +0100

    minor

--- src/math/calcFill.ts
+++ src/math/calcFill.ts
@@ -2,6 +2,27 @@ import { hexToRgb, rgbToHex } from '../utils/colorConverter';
 import { map } from './map';
 import { noise, seed, detail } from './simplexNoise';
 
+const { sin, cos, tan, floor, ceil, round } = Math;
+
+function calcNoise(x: number, y: number, c: number) {
+    let newC = new Function(
+        'x',
+        'y',
+        'c',
+        'noise',
+        'sin',
+        'cos',
+        'tan',
+        'floor',
+        'ceil',
+        'round',
+        `return ${fn.value}`
+    )(x, y, c, noise, sin, cos, tan, floor, ceil, round);
+    newC = map(newC, 0, 1, 0, 255);
+    newC = Math.round(newC);
+    return newC;
+}
+
 // do not remove x and y vars, they are needed for fn input
 export function calcFill(x: number, y: number, hex: string) {
     let newC = {
@@ -11,11 +32,4 @@ export function calcFill(x: number, y: number, hex: string) {
     };
 
     return rgbToHex(newC.r, newC.b, newC.g);
-}
-
-function calcNoise(x: number, y: number, c: number) {
-    let newC = new Function('noise', 'x', 'y', 'c', `return noise(${fn.value})`)(noise, x, y, c);
-    newC = map(newC, 0, 1, 0, 255);
-    newC = Math.round(newC);
-    return newC;
 }
